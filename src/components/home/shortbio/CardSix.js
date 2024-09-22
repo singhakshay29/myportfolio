9caf964bea1cd6942b8fb04a8e5d67d52d81146a
akshaysingh29 <akshay.singh@uniproworld.com>
2024-09-22 16:20:36 +0530

    added &apos

--- src/components/home/shortbio/CardSix.js
+++ src/components/home/shortbio/CardSix.js
@@ -9,11 +9,11 @@ export default function CardSix({ darkMode }) {
         : "bg-white "}  rounded-3xl  lg:p-10 min-h-[310px] flex flex-col justify-between hover:shadow-lg bg-cover bg-center bg-[url('../assets/doodle.png')]`}
     >
       <div className=" pt-8 font-pacifico font-bold text-4xl">
-        How it started vs. how it's going
+        How it started vs. how it&apos;s going
       </div>
       <div className=" font-[Ubuntu] text-2xl">
         A short personal history as it relates to design and development,and how
-        I've found value in the cross-section between both disciplines.
+        I&apos;ve found value in the cross-section between both disciplines.
       </div>
     </div>
   );
